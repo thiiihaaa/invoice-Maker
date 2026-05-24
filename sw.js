e9e152d1fbd1de5051ee03c590a02c00b03fd1fc
thiiihaaa <haruuki999@gmail.com>
2026-05-24 20:39:41 +0900

    Hide Google logo from sign-in button

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Cache-first for local, network-first for CDN
 ═══════════════════════════════════════ */
 
-const CACHE  = 'invoice-maker-v23';
+const CACHE  = 'invoice-maker-v24';
 const SHELL  = [
   './',
   './index.html',
